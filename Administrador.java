06a00562ed06a5815cf972ebca79e64a31a408af
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-05-16 19:58:40 +0000

    Completo

---
+++ Administrador.java
@@ -0,0 +1,19 @@
+public class Administrador extends Empregado{
+
+    private double ajudaCusto;
+
+    public double getAjudaCusto() {
+        return ajudaCusto;
+    }
+
+    public void setAjudaCusto(double ajudaCusto) {
+        this.ajudaCusto = ajudaCusto;
+    }
+
+    public double calcularSalario(double ajudaCusto){
+        double salarioLiquido;
+        salarioLiquido=calcularSalario()+ajudaCusto;
+        return salarioLiquido;
+    }
+    
+}
